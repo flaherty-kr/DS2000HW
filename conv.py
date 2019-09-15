373d57144d893a0909583645092bc506ff68fdd6
Kristen Flaherty <54455337+flaherty-kr@users.noreply.github.com>
2019-09-14 23:25:08 -0400

    Create conv.py

---
+++ conv.py
@@ -0,0 +1,12 @@
+# Kristen Flaherty
+# Sec 01
+
+#key parameters
+#input
+km = float(input('Please enter the number of kilometers:\n'))
+#km to miles conversion
+conv = .621
+full_miles = int(km * conv)
+
+#print full miles statement
+print("The number of full miles is:", full_miles)
